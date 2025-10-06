9b7fc4d9aebaed0381cbb924b55d850714204d8c
illicchpv <illich58@mail.ru>
2025-10-06 19:30:53 +0300

    +: button paragraph. title, logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import './App.css';
+import Button from './components/Button/Button';
+import Paragraph from './components/Paragraph/Paragraph';
+import Title from './components/Title/Title';
+
 function App() {
 
   return (
     <>
       <div>
-        start
+        <Button >Искать</Button>
+      </div>
+      <div>
+        <Paragraph size="large" >Введите название фильма, сериала или мультфильма для поиска и добавления в избранное.</Paragraph>
+        <Paragraph >Введите название фильма, сериала или мультфильма для поиска и добавления в избранное.</Paragraph>
+        <Paragraph size="small" >Введите название фильма, сериала или мультфильма для поиска и добавления в избранное.</Paragraph>
+      </div>
+      <div>
+        <Title as="h1" >h1 Поиск</Title>
+        <Title as="h2" >h2 Поиск</Title>
       </div>
     </>
   );
