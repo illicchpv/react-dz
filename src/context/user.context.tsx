5839d99e87b35f6ea07d43c8fa7eecc061d9b6d3
illicchpv <illich58@mail.ru>
2025-11-01 16:30:39 +0300

    исправление "Fast refresh only works when a file only exports components. Move your React context(s) to a separate file."

--- src/context/user.context.tsx
+++ src/context/user.context.tsx
@@ -1,24 +1,11 @@
-import {createContext, useState} from 'react';
+import { createContext } from 'react';
 
 export interface IUserContext {
   currentUserName: string;
-  setCurrentUserName?: (name: string) => void;
-}
-
-interface UserContextProviderProps {
-  children: React.ReactNode;
+  setCurrentUserName: (name: string) => void;
 }
 
 export const UserContext = createContext<IUserContext>({
-  currentUserName: ''
+  currentUserName: '',
+  setCurrentUserName: () => {} // заглушка
 });
-
-export const UserContextProvider = ({children}: UserContextProviderProps) => {
-  const [currentUserName, setCurrentUserName] = useState<string>('');
-
-  return (
-    <UserContext.Provider value={{currentUserName, setCurrentUserName}}>
-      {children}
-    </UserContext.Provider>
-  );
-};
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
-import { UserContextProvider } from './context/user.context';
 import App from './App';
+import { UserContextProvider } from './context/user.provider';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
